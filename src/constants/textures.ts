6223e9c7ac72b021e8cceb74ed98a423de73271b
Roman Borovik <starmordar@gmail.com>
2024-01-02 14:17:11 +0300

    Update wraith textures

--- src/constants/textures.ts
+++ src/constants/textures.ts
@@ -8,15 +8,29 @@ export enum TEXTURES {
 
 export enum MONSTER_SPRITES {
   idle = 'idle',
+  move = 'move',
+
   attackStraight = 'attackStraight',
+  attackUp = 'attackUp',
+  attackDown = 'attackDown',
+
   getHit = 'getHit',
+  defend = 'defend',
+  death = 'death',
 }
 
 const frames: Partial<TextureFrames> = {
   [TEXTURES.wraith]: {
-    [MONSTER_SPRITES.idle]: { y: 1, x: [0, 1, 2, 3, 4, 5, 6] },
-    [MONSTER_SPRITES.attackStraight]: { y: 0, x: [0, 1, 2, 3, 4, 5, 6] },
-    [MONSTER_SPRITES.getHit]: { y: 2, x: [0, 1, 2, 3, 4, 5] },
+    [MONSTER_SPRITES.idle]: { y: 6, x: [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6] },
+    [MONSTER_SPRITES.move]: { y: 6, x: [0, 1, 2, 3, 4, 5, 6] },
+
+    [MONSTER_SPRITES.attackStraight]: { y: 1, x: [0, 1, 2, 3, 4, 5, 6] },
+    [MONSTER_SPRITES.attackUp]: { y: 2, x: [0, 1, 2, 3, 4, 5, 6] },
+    [MONSTER_SPRITES.attackDown]: { y: 0, x: [0, 1, 2, 3, 4, 5, 6, 7] },
+
+    [MONSTER_SPRITES.getHit]: { y: 4, x: [0, 1, 2, 3, 4, 5] },
+    [MONSTER_SPRITES.defend]: { y: 5, x: [0, 1, 2, 3] },
+    [MONSTER_SPRITES.death]: { y: 3, x: [0, 1, 2, 3, 4, 5, 6, 7] },
   },
 };
 
